a9c9959c3838280adaf0feb8dcbc9cf2c52bd630
anushparulekar <anush.parulekar@leapfinance.com>
2024-07-21 22:37:39 +0530

    moved function to different function

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,8 +1,10 @@
 public class AddTwoNumbers {
 
     public static void main(String[] args) {
-        int num1 = 1;
-        int num2 = 2;
+        addTwoNumbers(1,3);
+    }
+
+    private static void addTwoNumbers(int num1, int num2) {
         System.out.println(num1 + num2);
     }
 
